ce053e29ca2851047ca7c1ca78fa26e33b0d0bf7
Edgar Quezada <edgar.quezada@gmail.com>
2025-05-27 12:18:18 +0200

    upload files to views folder

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-col p-8">
+
+    </div>
+</template>
+
+<script setup>
+    import { computed } from '@vue/reactivity';
+    import { onMounted, ref } from 'vue';
+    import store from '../store';
+    import axiosClient from '../axiosClient.js';
+
+    const ingredients = ref([])
+
+    onMounted(async () => {
+        const response = await axiosClient.get('/list.php?i=list')
+        console.log(response.data)
+        ingredients.value = response.data
+    })
+</script>
