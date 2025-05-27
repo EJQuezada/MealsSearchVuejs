c22d45386a23d1614ca3c70822db671829686e1b
Edgar Quezada <edgar.quezada@gmail.com>
2025-05-27 12:17:30 +0200

    upload files to store folder

---
+++ src/store/state.js
@@ -0,0 +1,8 @@
+import MealsByIngredient from "../views/MealsByIngredient.vue";
+import MealsByLetter from "../views/MealsByLetter.vue";
+
+export default {
+    searchedMeals: [],
+    MealsByLetter: [],
+    MealsByIngredient: [],
+}
