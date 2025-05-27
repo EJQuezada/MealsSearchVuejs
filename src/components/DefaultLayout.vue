9f60a24e9bc34fe3582659e87f19275f04607958
Edgar Quezada <edgar.quezada@gmail.com>
2025-05-27 12:15:56 +0200

    upload files to components folder

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navbar from './Navbar.vue';
+
+</script>
+
+<template>
+    <div class="bg-gray-100 h-[100vh]">
+        <Navbar />
+        <main>
+            <router-view />
+        </main>
+  </div>
+</template>
