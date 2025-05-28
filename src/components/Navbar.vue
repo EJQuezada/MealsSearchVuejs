6ad4cefb556ad1a0099f3d6463bfb20538f49da4
Edgar Quezada <edgar.quezada@gmail.com>
2025-05-28 09:30:34 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,26 +2,26 @@
     <header class="bg-white shadow h-16 flex justify-between items-stretch">
       <router-link 
         :to="{name: 'home'}" 
-        class="px-5 inline-flex items-center h-full"
+        class="px-5 inline-flex items-center h-full text-orange-500 font-bold"
       >
         Home
       </router-link>
       <div class="flex items-center gap-2">
         <router-link 
           :to="{name: 'byName'}" 
-          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
         >
           Search Meals
         </router-link>  
         <router-link 
           :to="{name: 'byLetter'}" 
-          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
         >
           Meals by Letter
         </router-link>  
         <router-link 
           :to="{name: 'ingredients'}" 
-          class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+          class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
         >
           Meals by Ingredients
         </router-link>  
