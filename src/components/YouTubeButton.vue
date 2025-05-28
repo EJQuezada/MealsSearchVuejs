a9db518fd521862a4c32f1d3a2bb6f8b753436f2
Edgar Quezada <edgar.quezada@gmail.com>
2025-05-28 09:31:35 +0200

    Update YouTubeButton.vue

--- src/components/YouTubeButton.vue
+++ src/components/YouTubeButton.vue
@@ -2,7 +2,7 @@
     <a 
         :href="href" 
         target="_blank" 
-        class="font-semibold px-3 py-2 rounded border-2 border-red-600 bg-red-500 hover:bg-red-600 text-white transition-colors"
+        class="font-semibold px-3 py-2 rounded border-2 border-orange-600 bg-orange-500 hover:bg-orange-600 text-white transition-colors"
     >
     <slot></slot>
     </a> 
