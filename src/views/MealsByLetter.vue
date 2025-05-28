01a5830ff9d984247f0f649958a612876ddf593d
Edgar Quezada <edgar.quezada@gmail.com>
2025-05-28 09:40:44 +0200

    Update MealsByLetter.vue

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="flex justify-center gap-2 mt-2">
+    <div class="p-8 pb-0">
+        <h1 class="text-4xl font-bold mb-4 text-orange-500">Meals by Letter</h1>
+    </div>
+    <div class="flex flex-wrap justify-center gap-2 px-8 mb-6 mt-2">
         <router-link 
-            :to="{name: 'byLetter', params: { letter } }" 
+            :to="{ name: 'byLetter', params: { letter } }" 
             v-for="letter of letters" 
             :key="letter"
+            class="w-2 h2 flex items-center justify-center hover:text-orange-500 hover:scale-150 transition-all"
         >
             {{ letter }}
         </router-link>
@@ -19,16 +23,15 @@ import store from '../store';
 import { useRoute } from 'vue-router';
 import Meals from '../components/Meals.vue';
 
-    const route = useRoute();
-    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
-    const meals = computed(() => store.state.mealsByLetter)
+const route = useRoute();
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+const meals = computed(() => store.state.mealsByLetter);
 
-    watch(route, () => {
-      store.dispatch('searchMealsByLetter', route.params.letter)  
-    })
+watch(route, () => {
+    store.dispatch('searchMealsByLetter', route.params.letter);  
+});
 
-    onMounted(() => {
-      store.dispatch('searchMealsByLetter', route.params.letter)  
-    })
-
+onMounted(() => {
+    store.dispatch('searchMealsByLetter', route.params.letter);  
+});
 </script>
