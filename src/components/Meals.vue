0d204f732d992312afd221d8b8bd52744e3a1c28
Edgar Quezada <edgar.quezada@gmail.com>
2025-05-28 09:27:47 +0200

    Update Meals.vue

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
         <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
     </div>
-    <div v-if="!meals.length" class="flex justify-center text-gray-600">
+    <div v-if="!meals" class="flex justify-center text-gray-600">
         There are no meals
     </div>
 </template>
@@ -14,7 +14,6 @@ import MealItem from './MealItem.vue';
         meals: {
             required: true,
             type: Array,
-            default: () => []
         }
     })
 
